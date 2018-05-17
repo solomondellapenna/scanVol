31a2637578bf2866d2e5d0a19095df1547afc698
Solomon DellaPenna <solomondellapenna@gmail.com>
2018-05-17 11:24:27 -0500

    you're welome emiline

--- pqiwnef.sh
+++ pqiwnef.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-rawaws=$(aws ec2 describe-volumes | grep VolumeId)
-listaws=$(awk '/vol-\w+\d+\w+/ $rawaws }')
-echo $listaws
+export KUBECONFIG=~/Downloads/dev-kubeconfig_yaml.yaml
+rawaws=$(aws ec2 describe-volumes | egrep -o 'vol-\w+')
+rawkube=$(kubectl describe pv | egrep -o 'vol-\w+')
+
+[[ $rawaws in $rawkube ]]
+echo "${BASH_REMATCH[1]}"
+
+# echo $rawaws
+# echo $rawkube
