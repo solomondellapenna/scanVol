a5ad91e90de105244f5e7f74c9847aabf43054e0
Solomon DellaPenna <solomondellapenna@gmail.com>
2018-05-18 11:42:59 -0500

    Final Product, slack integration still needed

--- finalScan.sh
+++ finalScan.sh
@@ -4,29 +4,24 @@ rawAWS=$(aws ec2 describe-volumes | jq '.Volumes[] | .State, .VolumeId')
 AWS="$(echo "$rawAWS" | fgrep -xv '"in-use"')"
 final="$(echo "$AWS" | egrep -A 1 -o '"available"')"
 rawKUBE=$(kubectl describe pv | egrep -o 'vol-\w+')
-
-# ARR=()
-# for i in "${arr[@]}"
-# do
-#     ARR+=("$(echo "$i" | fgrep -exv '"vol-\w+"')")
-# done
-echo $final
-
-
-
-
-#sed "/^in-use/d" $rawAWS
-# echo $rawAWS
-# AWS=$(echo $rawAWS | grep -o '"State".*')
-# if [[ -z $AWS ]]
-# then
-#     echo "help"
-# else
-#     echo $AWS
-# fi
-
-#IF state exists and if does not equal in-use then print with 
-
-
-#Parse out "in-use"
-#If anything doesnt match vol- print the preceeding id
+if [ -z "$final" ]
+then 
+    echo "All in use"
+    exit 0
+else  
+    arr=($rawKUBE)
+    ARR=()
+    for i in "${arr[@]}"
+    do
+        ARR+="$(echo $final | egrep -o $i)"
+    done
+    if [ -z "$ARR" ]
+    then
+        delete="$(echo $final | egrep -o 'vol-\w+')"
+        echo "'$delete' should be deleted"
+        exit 1
+    else 
+        echo "Available volumes listed in Kubernetes"
+        exit 0
+    fi
+fi
