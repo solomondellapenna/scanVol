e1c935098f442abf408eb49b76064e4dd6149ebd
Solomon DellaPenna <solomondellapenna@gmail.com>
2018-05-18 14:23:34 -0500

    Finishing touches

--- finalScan.sh
+++ finalScan.sh
@@ -6,6 +6,7 @@ final="$(echo "$AWS" | egrep -A 1 -o '"available"')"
 rawKUBE=$(kubectl describe pv | egrep -o 'vol-\w+')
 if [ -z "$final" ]
 then 
+    #No "Available" volumes, all in use
     echo "All in use"
     exit 0
 else  
@@ -17,10 +18,12 @@ else
     done
     if [ -z "$ARR" ]
     then
+        #"Available" volume found and not referenced in kubernetes pv
         delete="$(echo $final | egrep -o 'vol-\w+')"
-        echo "'$delete' should be deleted"
+        echo "$delete should be deleted"
         exit 1
     else 
+        #"Available" volume found but referenced in kubernetes pv
         echo "Available volumes listed in Kubernetes"
         exit 0
     fi
