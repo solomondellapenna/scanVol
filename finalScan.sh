10f1cec13f7acc9ec32ecd81e87336553e6f1125
Solomon DellaPenna <solomondellapenna@gmail.com>
2018-05-18 11:02:58 -0500

    You're welcome again

--- finalScan.sh
+++ finalScan.sh
@@ -1,7 +1,22 @@
+#!/usr/bin/env bash
+export KUBECONFIG=~/Downloads/dev-kubeconfig_yaml.yaml
 rawAWS=$(aws ec2 describe-volumes | jq '.Volumes[] | .State, .VolumeId')
-#egrep -o -A 2 --group-separator=$'\n' '"SnapshotId".*')
+AWS="$(echo "$rawAWS" | fgrep -xv '"in-use"')"
+final="$(echo "$AWS" | egrep -A 1 -o '"available"')"
+rawKUBE=$(kubectl describe pv | egrep -o 'vol-\w+')
 
-echo $rawAWS
+# ARR=()
+# for i in "${arr[@]}"
+# do
+#     ARR+=("$(echo "$i" | fgrep -exv '"vol-\w+"')")
+# done
+echo $final
+
+
+
+
+#sed "/^in-use/d" $rawAWS
+# echo $rawAWS
 # AWS=$(echo $rawAWS | grep -o '"State".*')
 # if [[ -z $AWS ]]
 # then
@@ -11,3 +26,7 @@ echo $rawAWS
 # fi
 
 #IF state exists and if does not equal in-use then print with 
+
+
+#Parse out "in-use"
+#If anything doesnt match vol- print the preceeding id
